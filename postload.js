1cbf3dfee0ab104df8042a89ce069cbb15ff4539
Alexandr Kobets <sasha.kobec00@gmail.com>
2020-03-10 21:23:06 +0300

    Prepare a code for mass replacements
    Make constant object which loop and overwrite track_list

--- postload.js
+++ postload.js
@@ -1,27 +1,26 @@
 ig.module('game.feature.bgm.replace-music')
   .requires("game.feature.bgm.playlist")
   .defines(function () {
-  	//ig.merge(ig.BGM_TRACK_LIST, { metalGear: { path: "media/bgm/metalGear1.mp3", loopEnd: 113.777, volume: 0.5 } });
-    ig.BGM_TRACK_LIST.basinKeep = { path: "media/bgm/CelesteBasin3.mp3", loopEnd: 155, volume: 1 };
-    ig.BGM_TRACK_LIST.emilie = { path: "media/bgm/ValseSonic4.mp3", loopEnd: 69, volume: 0.8 };
-    ig.BGM_TRACK_LIST.bergenVillage = { path: "media/bgm/ValseSonic4.mp3", loopEnd: 69, volume: 0.8 };
-    ig.BGM_TRACK_LIST.metalGear = { path: "media/bgm/metalGear1.mp3", loopEnd: 113.777, volume: 0.5 };
-    ig.BGM_DEFAULT_TRACKS.metalGear = {
-			field: {
-				track: "metalGear",
-				volume: 1
-			}, battle: { track: "tutorial-battle", volume: 1 }, rankBattle: { track: "fieldBattle", volume: 1 }, sRankBattle: { track: "s-rank", volume: 1 }
-		};
-    /*
-     rookieHarbor: {
-			field: {
-				track: "rookieHarbor",
-				volume: 1
-			}, battle: { track: "tutorial-battle", volume: 1 }, rankBattle: { track: "fieldBattle", volume: 1 }, sRankBattle: { track: "s-rank", volume: 1 }
-		}
-		*/
-    //infiltration: { path: "media/bgm/muInfiltration.mp3", loopEnd: 113.777, volume: 0.5 }
 
-    // basinKeep: { path: "media/bgm/muBasinkeep.mp3", loopEnd: 102.893, volume: 0.5 }
-    //  emilie: { path: "media/bgm/muValse.mp3", loopEnd: 62.905, volume: 0.5, introPath: "media/bgm/muValse-i.mp3", introEnd: 2.167 }  
-});
+    const tracks = {
+      basinKeep: { path: "media/bgm/CelesteBasin3.mp3", loopEnd: 155, volume: 1 },
+      emilie: { path: "media/bgm/ValseSonic4.mp3", loopEnd: 69, volume: 0.8 },
+      bergenVillage: { path: "media/bgm/ValseSonic4.mp3", loopEnd: 69, volume: 0.8 },
+      metalGear: { path: "media/bgm/metalGear1.mp3", loopEnd: 113.777, volume: 0.5 }
+    }
+
+    for (var i in tracks) {
+      console.log(tracks[i]);
+      ig.BGM_TRACK_LIST[i] = tracks[i];
+    }
+
+    /* 
+    ig.BGM_DEFAULT_TRACKS.metalGear = {
+      field: {
+        track: "metalGear",
+        volume: 1
+      }, battle: { track: "tutorial-battle", volume: 1 }, rankBattle: { track: "fieldBattle", volume: 1 }, sRankBattle: { track: "s-rank", volume: 1 }
+    }; */
+
+    //infiltration: { path: "media/bgm/muInfiltration.mp3", loopEnd: 113.777, volume: 0.5 }
+  });
