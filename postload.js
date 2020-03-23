d790e04c5b127ebd6cf23ce03d5218ef5acde5b7
Sandorik00 <sasha.kobec00@gmail.com>
2020-03-23 23:01:10 +0300

    started implement preload music replace

--- postload.js
+++ postload.js
@@ -6,8 +6,11 @@ ig.module('game.feature.bgm.replace-music')
       basinKeep: { path: "media/bgm/CelesteBasin7.mp3", loopEnd: 155.208, volume: 0.8 },
       emilie: { path: "media/bgm/ValseSonic4.mp3", loopEnd: 69, volume: 0.8 },
       bergenVillage: { path: "media/bgm/metalGear3.mp3", loopEnd: 94, volume: 0.8 },
-      metalGear: { path: "media/bgm/metalGear1.mp3", loopEnd: 113.777, volume: 0.5 }
+      metalGear: { path: "media/bgm/metalGear1.mp3", loopEnd: 113.777, volume: 0.5 },
+      rookieHarbor: { path: "media/bgm/ApolloRight.mp3", loopEnd: 76.825, volume: 0.8 },
+      apolloTheme: { path: "media/bgm/ApolloRight.mp3", loopEnd: 76.825, volume: 1 }
     }
+    //introPath: "media/bgm/muApollo-i.mp3", introEnd: 10.758
 
     for (var i in tracks) {
       console.log(tracks[i]);
--- prestart.js
+++ prestart.js
@@ -1,14 +1,26 @@
-ig.module('game.feature.custom-maps-bgm')
-    .requires("impact.base.loader")
-    .defines(function () {
-        ig.JsonLoadable.inject({
-            init: function (a) { this.parent(a); if (typeof a == "object") this.onload(a) }, loadInternal: function () { $.ajax({ dataType: "json", url: ig.getFilePath(this.getJsonPath()), context: this, success: this.onJsonLoaded.bind(this), error: this.onJsonError.bind(this) }) }, onJsonLoaded: function (a) {
-                //console.log(a);
-                a = ig.jsonTemplate.resolve(a); if (!a) {
-                    throw Error("Json file of path '" + this.path + "' is null. Maybe the original JSON had a syntax error and was compressed incorrectly?")
-                }
-                ig.activateCollectors(this); this.onload(a); ig.removeCollectors(this); this.loadingFinished(true)
-            }, onJsonError: function () { this.onerror && this.onerror(); this.loadingFinished(false) }
-        });
-    });
+ig.module('game.feature.loadable-replace')
+  .requires("impact.base.loader", "impact.feature.bgm.bgm")
+  .defines(function () {
+    ig.Bgm.inject({
+      onLevelLoadStart: function(a) {
+        this.clearResumeOnChange();
+        if (a.attributes && a.attributes.bgm) {
+          this.mapDefaultTrackSet && this.mapDefaultTrackSet.clearCached();
+          console.log(a.attributes.bgm);
+          this.mapDefaultTrackSet = this.loadTrackSet(a.attributes.bgm);
+          if (a.attributes.bgm == "autumn") {
+            this.mapDefaultTrackSet = this.loadTrackSet("bergenVillage");
+          }
+          this.setDefault(this.mapDefaultTrackSet, null, true);
+        }
+      }
+    })
     
+    /* ig.JsonLoadable.inject({
+      init: function(a) {
+        this.parent(a);
+        console.log(a);
+        if (typeof a == "object") this.onload(a);
+      }
+    }) */
+  })
