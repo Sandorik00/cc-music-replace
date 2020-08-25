c5142eb01c44e71e7a0f74bbef68a4d498f08267
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-08-26 00:35:24 +0300

    lookup event steps dynamically instead of using hardcoded indexes

--- mapPatches.js
+++ mapPatches.js
@@ -1,116 +1,135 @@
 ccmod.resources.jsonPatches.add("data/maps/autumn/path-1-3.json", (data) => {
-  data.entities[284].settings.event[19] = {
-    bgm: "bluehedgeheg",
-    volume: 1,
-    mode: "IMMEDIATELY",
-    type: "PUSH_BGM",
-  };
+  let entity = data.entities.find(
+    (ent) => ent.type === "EventTrigger" && ent.settings.name === "StartBoss"
+  );
+  let pushBgmEvent = entity.settings.event.find(
+    (evt) => evt.type === "PUSH_BGM" && evt.bgm === "challenge2"
+  );
+  pushBgmEvent.bgm = "bluehedgeheg";
 });
 
 ccmod.resources.jsonPatches.add(
   "data/maps/jungle/caves/infested-cave-02.json",
   (data) => {
-    data.entities[173].settings.event[29] = {
-      bgm: "sephisloth",
-      volume: 1,
-      mode: "IMMEDIATELY",
-      type: "PUSH_BGM",
-    };
+    let entity = data.entities.find(
+      (ent) => ent.type === "EventTrigger" && ent.settings.name === "startSloth"
+    );
+    let pushBgmEvent = entity.settings.event.find(
+      (evt) => evt.type === "PUSH_BGM" && evt.bgm === "challenge"
+    );
+    pushBgmEvent.bgm = "sephisloth";
   }
 );
 
 ccmod.resources.jsonPatches.add("data/maps/arid/tower-1.json", (data) => {
-  data.entities[242].settings.event[31] = {
-    bgm: "stealth",
-    volume: 1,
-    mode: "IMMEDIATELY",
-    type: "PUSH_BGM",
-  };
+  let entity = data.entities.find(
+    (ent) => ent.type === "EventTrigger" && ent.settings.name === "StealthStart"
+  );
+  let pushBgmEvent = entity.settings.event.find(
+    (evt) => evt.type === "PLAY_BGM" && evt.bgm === "infiltration"
+  );
+  pushBgmEvent.bgm = "stealth";
 });
 
 ccmod.resources.jsonPatches.add(
   "data/maps/jungle/grove/lost-shrine-04.json",
   (data) => {
-    data.entities[190].settings.event[21] = {
-      bgm: "jungleFish",
-      volume: 1,
-      mode: "IMMEDIATELY",
-      type: "PUSH_BGM",
-    };
+    let entity = data.entities.find(
+      (ent) => ent.type === "EventTrigger" && ent.settings.name === "BossStart"
+    );
+    let pushBgmEvent = entity.settings.event.find(
+      (evt) => evt.type === "PUSH_BGM" && evt.bgm === "challenge"
+    );
+    pushBgmEvent.bgm = "jungleFish";
   }
 );
 
 ccmod.resources.jsonPatches.add(
   "data/maps/forest/caves/cave-013-pandza-02.json",
   (data) => {
-    data.entities[101].settings.event[41] = {
-      bgm: "panzaMK2",
-      volume: 1,
-      mode: "IMMEDIATELY",
-      type: "PUSH_BGM",
-    };
+    let entity = data.entities.find(
+      (ent) => ent.type === "EventTrigger" && ent.settings.name === "BossStart"
+    );
+    let pushBgmEvent = entity.settings.event.find(
+      (evt) => evt.type === "PUSH_BGM" && evt.bgm === "challenge"
+    );
+    pushBgmEvent.bgm = "panzaMK2";
   }
 );
 
 ccmod.resources.jsonPatches.add(
   "data/maps/bergen-trail/cave/cave-6-3.json",
   (data) => {
-    data.entities[10].settings.event[10] = {
-      bgm: "pengDakara",
-      volume: 1,
-      mode: "IMMEDIATELY",
-      type: "PUSH_BGM",
-    };
+    let entity = data.entities.find(
+      (ent) =>
+        ent.type === "EventTrigger" && ent.settings.name === "startBattle"
+    );
+    let pushBgmEvent = entity.settings.event.find(
+      (evt) => evt.type === "PUSH_BGM" && evt.bgm === "challenge"
+    );
+    pushBgmEvent.bgm = "pengDakara";
   }
 );
 
 ccmod.resources.jsonPatches.add(
   "data/maps/heat/caves/cliffs-temple-03.json",
   (data) => {
-    data.entities[8].settings.event[40] = {
-      bgm: "sandShark",
-      volume: 1,
-      mode: "IMMEDIATELY",
-      type: "PUSH_BGM",
-    };
+    let entity = data.entities.find(
+      (ent) => ent.type === "EventTrigger" && ent.settings.name === "StartFight"
+    );
+    let pushBgmEvent = entity.settings.event.find(
+      (evt) => evt.type === "PUSH_BGM" && evt.bgm === "challenge"
+    );
+    pushBgmEvent.bgm = "sandShark";
   }
 );
 
 ccmod.resources.jsonPatches.add(
   "data/maps/heat/special/glitch-boss.json",
   (data) => {
-    data.entities[2].settings.event[14] = {
-      bgm: "USE_THIS",
-      volume: 1,
-      mode: "IMMEDIATELY",
-      type: "PUSH_BGM",
-    };
+    let entity = data.entities.find(
+      (ent) => ent.type === "EventTrigger" && ent.settings.name === "bossStart"
+    );
+    let pushBgmEvent = entity.settings.event.find(
+      (evt) => evt.type === "PLAY_BGM" && evt.bgm === "challenge2"
+    );
+    pushBgmEvent.bgm = "USE_THIS";
   }
 );
 
 ccmod.resources.jsonPatches.add(
   "data/maps/jungle/caves/smuggle-route-2.json",
   (data) => {
-    data.entities[3].settings.npcStates[1].event.quest[3][0][0] = {
-      bgm: "stealth",
-      mode: "MEDIUM_OUT",
-      type: "PUSH_BGM",
-      volume: 1,
-    };
+    let entity = data.entities.find(
+      (ent) => ent.type === "NPC" && ent.settings.name === "mick"
+    );
+    let npcState = entity.settings.npcStates.find(
+      (st) => st.condition === "quest.basin-mush-2.task.2"
+    );
+    let events = npcState.event.quest;
+
+    let choiceEvent = events.find((evt) => evt.type === "SHOW_CHOICE");
+    // TODO: Is it possible to find this branch without guessing?
+    let yesChoiceEvents = choiceEvent[0];
+    let pushBgmEvent = yesChoiceEvents.find(
+      (evt) => evt.type === "PUSH_BGM" && evt.bgm === "infiltration"
+    );
+    pushBgmEvent.bgm = "stealth";
   }
 );
 
 ccmod.resources.jsonPatches.add(
   "data/enemies/jungle/special/guard.json",
   (data) => {
-    data.actions.Detect.splice(2, 0, {
+    let detectAction = data.actions.Detect;
+    detectAction.splice(2, 0, {
       volume: 1,
       global: false,
       loop: false,
       type: "PLAY_SOUND",
       sound: "media/sound/alertsoundmgs.ogg",
     });
-    data.actions.Detect.splice(8, 0, {
+    detectAction.splice(8, 0, {
       time: -1,
       type: "WAIT",
     });
